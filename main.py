7e6e97dd743524793dc6fc55685afb0addb8c094
Danish Siddiqui <64173264+not-danish@users.noreply.github.com>
2022-11-08 17:25:57 +0000

    added datatocsv and fields functions

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import worldcup
+
 #the season_id for the 2018 world cup is 892
 
-#gets 
-print(worldcup.teamgoals(18647,892))
+#gets all the players who scored goals for a specific team (team_id) for a specific year (season_id)
+#goals = worldcup.teamgoals(18647,892)
+#print(goals)
+#worldcup.datatocsv(goals,"goals.csv")
 
 print("----------------------------------------")
 print("----------------------------------------")
 
 #get information on a specific player based on their player_id
-#print(worldcup.player(1339))
 
 print("----------------------------------------")
 print("----------------------------------------")
 
 #gets all of the top scorers for the 2018 world cup
-#print(worldcup.topscorers(892))
+topscorers = worldcup.topscorers(892)
+print(topscorers)
+worldcup.datatocsv(topscorers, "scorers.csv")
--- worldcup.py
+++ worldcup.py
@@ -1,6 +1,8 @@
+#This script was created by Danish Siddiqui for MTA's Python Event.
+from typing import List, Dict
 import requests
 import json
-
+import csv
 
 def getapiurl(endpoint: str,parameters: dict) -> str:
   
@@ -24,20 +26,49 @@ def matchgoals(fixture_id: int) -> dict:
   data = datatodict(url)
   return data
   
-def teamgoals(team_id: int, season_id: int) -> dict:
+def teamgoals(team_id: int, season_id: int) -> List[Dict]:
   #gets all the goals scored by all the players in a specified team (team_id) in a specified year (season_id)
   url = getapiurl(f"teams/{team_id}",{"include":"goalscorers","seasons":season_id})
   data = datatodict(url)
   return data['data']['goalscorers']['data']
 
-def player(player_id: int) -> list:
+def player(player_id: int) -> List[Dict]:
   #gets info on players based on player_id
   url = getapiurl(f"players/{player_id}", {})
   data = datatodict(url)
-  return data['data']
+  if 'meta' in data:
+    data.pop('meta')
+  else:
+    data = {}
+  return data
 
 def topscorers(season_id: int) -> dict:
-  #gets all of the top scorers for the world cup
+  #gets top 25 scorers for the world cup season (season_id)
+  url = getapiurl(f"topscorers/season/{season_id}",{"include":"goalscorers.player.team"})
+  data = datatodict(url)
+  return data['data']['goalscorers']['data']
+
+def topassist(season_id: int) -> dict:
+  #gets top 25 assisters for the world cup season (season_id)
   url = getapiurl(f"topscorers/season/{season_id}",{"include":"goalscorers.player.team"})
   data = datatodict(url)
   return data['data']
+
+def fields(data: dict) -> list:
+  #gets all of the fields for a specific dict
+  fields = []
+  for key in data[0]:
+    fields.append(key)
+  return fields
+
+def datatocsv(data: list, filename: str) -> None:
+  #converts the dict data to a csv with name filename
+  columns = fields(data)
+  try:
+      with open(filename, 'w') as csvfile:
+          writer = csv.DictWriter(csvfile, fieldnames=columns)
+          writer.writeheader()  
+          writer.writerows(data)
+  except IOError:
+      print("I/O error")
+  
